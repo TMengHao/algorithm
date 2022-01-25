54d38c51ab655928bf534ad6b4be41429f887043
MengHao Tian <907749695@qq.com>
2022-01-25 21:23:26 +0800

    feat:打印希尔排序过程

--- PrintSort.h
+++ PrintSort.h
@@ -4,42 +4,51 @@
 #include <cstdio>
 
 /*
+	打印直接插入排序过程
+*/
+void PrintInsertSort(int arr[], int n);
+
+/*
+	打印希尔排序过程
+*/
+void PrintShellSort(int arr[], int n, int inc[], int cn);
+
+/*
 	arr:待输出数组
 	n:数组长度(sizeof(arr)/sizeof(arr[0]))
 */
 void PrintArray(int arr[], int n);
+
 /*
 	arr:待格式化数组(闭区间)
 	start:开始位置
 	end:结束位置
 */
 void FormatArray(int arr[], int start, int end);
-/*
-	arr:原数组
-	n:数组长度
-	打印直接插入排序过程
-*/
-void PrintInsertSort(int arr[], int n);
 
-
-void PrintArray(int arr[], int n) {
-	for (int i = 0; i < n; ++i) {
-		printf("%d\t", arr[i]);
-	}
-}
-void FormatArray(int arr[], int start, int end) {
-	for (int i = start; i <= end; ++i) {
-		if (i == start) printf("[");
-		if (i != end) printf("%d,", arr[i]);
-		if (i == end) {
-			printf("%d", arr[i]);
-			printf("]");
+void PrintShellSort(int arr[], int n, int inc[], int cn) {
+	printf("初始数组: ");
+	PrintArray(arr, n); printf("\n");
+	for (int k = 0; k < cn; ++k) {
+		int i, j, temp, ck = inc[k];
+		for (i = ck; i < n; ++i) {
+			if (arr[i] < arr[i - ck]) {
+				temp = arr[i];
+				j = i - ck;
+				while (j >= 0 && temp < arr[j]) {
+					arr[j + ck] = arr[j];
+					j -= ck;
+				}
+				arr[j + ck] = temp;
+			}
 		}
+		printf("增量为 %d 排序后: ", inc[k]);
+		PrintArray(arr, n); printf("\n");
 	}
 }
 void PrintInsertSort(int arr[], int n) {
 	int i, j, temp;
-	printf("初始化数据：");
+	printf("初始数组:");
 	FormatArray(arr, 0, 0);
 	FormatArray(arr, 1, n - 1);
 	printf("\n");
@@ -57,5 +66,20 @@ void PrintInsertSort(int arr[], int n) {
 		printf("\n");
 	}
 }
+void PrintArray(int arr[], int n) {
+	for (int i = 0; i < n; ++i) {
+		if (i != n - 1) printf("%d,", arr[i]);
+		else printf("%d", arr[i]);
+	}
+}
+void FormatArray(int arr[], int start, int end) {
+	for (int i = start; i <= end; ++i) {
+		if (i == start) printf("[");
+		if (i != end) printf("%d,", arr[i]);
+		else {
+			printf("%d", arr[i]);
+			printf("]");
+		}
+	}
+}
 #endif // !_PRINT_SORT_H
-
