616ca18eef9ea5906b4fe2877e7abf587f793630
MengHao Tian <907749695@qq.com>
2022-01-25 19:59:25 +0800

    fix:分离打印函数

--- InsertSort.h
+++ InsertSort.h
@@ -1,17 +1,12 @@
 #ifndef _INSERT_SORT_H
 #define _INSERT_SORT_H
-
+//直接插入排序
 #include <cstdio>
 
 void InsertSort(int arr[], int n);
-void PrintSort(int arr[], int start, int end);
 
 void InsertSort(int arr[], int n) {
 	int i, j, temp;
-	printf("初始化数据：");
-	PrintSort(arr,0,0);
-	PrintSort(arr,1,n-1);
-	printf("\n");
 	for (i = 1; i <= n - 1; ++i) {
 		if (arr[i] < arr[i - 1]) {
 			temp = arr[i];
@@ -19,20 +14,6 @@ void InsertSort(int arr[], int n) {
 				arr[j + 1] = arr[j];
 			}
 			arr[j + 1] = temp;
-		}
-		printf("%d次排序后: ",i);
-		PrintSort(arr,0,i);
-		PrintSort(arr,i+1,n-1);
-		printf("\n");
-	}
-}
-void PrintSort(int arr[], int start, int end) {
-	for (int i = start; i <= end; ++i) {
-		if(i == start) printf("[");
-		if(i != end) printf("%d,", arr[i]);
-		if(i == end) {
-			printf("%d", arr[i]);
-			printf("]");
 		}
 	}
 }
