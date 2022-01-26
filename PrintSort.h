f04b274f717ba2f4ff544389794a934b48f04cb4
MengHao Tian <907749695@qq.com>
2022-01-26 14:22:35 +0800

    feat:增加快速排序

--- PrintSort.h
+++ PrintSort.h
@@ -4,31 +4,31 @@
 #include <cstdio>
 
 /*
-	打印直接插入排序过程
-*/
+ * 打印直接插入排序过程
+ * */
 void PrintInsertSort(int arr[], int n);
 
 /*
-	打印希尔排序过程
-*/
+ * 打印希尔排序过程
+ * */
 void PrintShellSort(int arr[], int n, int inc[], int cn);
 
 /*
-	打印冒泡排序过程
-*/
+ * 打印冒泡排序过程
+ * */
 void PrintBubbleSort(int arr[], int n);
 
 /*
-	arr:待输出数组
-	n:数组长度(sizeof(arr)/sizeof(arr[0]))
-*/
+ * arr:待输出数组
+ * n:数组长度(sizeof(arr)/sizeof(arr[0]))
+ * */
 void PrintArray(int arr[], int n);
 
 /*
-	arr:待格式化数组(闭区间)
-	start:开始位置
-	end:结束位置
-*/
+ * arr:待格式化数组(闭区间)
+ * start:开始位置
+ * end:结束位置
+ * */
 void FormatArray(int arr[], int start, int end);
 
 /*-----------------------------------------------------------------------------------*/
